9b32acd22a49075783aa3a6812e55214f42b052b
Blas <d.moralesblasescom@gmail.com>
2023-05-07 18:49:36 -0600

    Se hacen cambios a modulo general, y angular json para modulos

--- Kirana/src/app/app.component.ts
+++ Kirana/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IndexLogicService } from './services/index-logic.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Kirana';
+
+  constructor(private indexLogicService:IndexLogicService){
+  }
+  getIsAccepted(){
+    return this.indexLogicService.getIsAccepted();
+  }
 }
--- Kirana/src/app/app.module.ts
+++ Kirana/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UpfileComponent } from './components/upfile/upfile.component';
+import { TableCSVComponent } from './components/table-csv/table-csv.component';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from  '@angular/material/paginator';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UpfileComponent,
+    TableCSVComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxDropzoneModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
